afb09080f3f8381441ae479fa407b579212d2c67
Alexander Hansson <ak.hansson88@gmail.com>
2026-03-13 16:42:36 +0000

    Remove Referens from header navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,6 @@ function Header() {
             <NavLink to="/" end className={({ isActive }) => (isActive ? 'active' : undefined)}>Hem</NavLink>
             <NavLink to="/produkter" className={({ isActive }) => (isActive ? 'active' : undefined)}>Produkter</NavLink>
             <NavLink to="/galleri" className={({ isActive }) => (isActive ? 'active' : undefined)}>Galleri</NavLink>
-            <a href="#">Referens</a>
             <NavLink to="/om-oss" className={({ isActive }) => (isActive ? 'active' : undefined)}>Om oss</NavLink>
             <NavLink to="/reklam" className={({ isActive }) => (isActive ? 'active' : undefined)}>Reklam</NavLink>
             <a href="#">Blog</a>
